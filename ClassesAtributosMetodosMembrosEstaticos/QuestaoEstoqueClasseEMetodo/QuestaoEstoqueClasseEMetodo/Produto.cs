612779f02ed653c0aaf46872d4a753d3285d931e
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-06-23 22:32:54 -0300

    Entrada e saida de estoque com classe e método (inacabado0

---
+++ ClassesAtributosMetodosMembrosEstaticos/QuestaoEstoqueClasseEMetodo/QuestaoEstoqueClasseEMetodo/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuestaoEstoqueClasseEMetodo {
+    internal class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Quantidade * Preco;
+            
+        }
+        public void AdicionarProdutos() {
+
+        }
+        public void RemoverProdutos() {
+
+        }
+    }
+}
---
+++ ClassesAtributosMetodosMembrosEstaticos/QuestaoEstoqueClasseEMetodo/QuestaoEstoqueClasseEMetodo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace QuestaoEstoqueClasseEMetodo {
+    internal class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto();
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            double valorTotalP = p.ValorTotalEmEstoque();
+            Console.Write($"Dados do produto:{p.Nome}, ${p.Preco.ToString("F2", CultureInfo.InvariantCulture)}, {p.Quantidade} unidades,Total: $" + valorTotalP);
+        }
+    }
+}
