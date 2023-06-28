d1bdea4b61f9775a91495b3e76615aa33f18bbe7
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-06-28 20:58:12 -0300

    Metodo Get e Set (Encapsulamento)

---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Encapsulamento {
+    internal class Program {
+        static void Main(string[] args) {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(p.GetNome());
+            string nome = Console.ReadLine();
+            p.SetNome(nome); 
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetQuantidade());
+            Console.WriteLine(p.GetPreco());
+        }
+
+    }
+}
