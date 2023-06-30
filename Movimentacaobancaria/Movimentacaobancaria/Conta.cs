8a5853c76aaf1bd166208e23ae476fa70539be92
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-06-29 21:29:58 -0300

    Questão de movimentação Bancária

---
+++ Movimentacaobancaria/Movimentacaobancaria/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Movimentacaobancaria {
+    internal class Conta {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public Conta(string nome, int numeroConta) {
+            Nome = nome;
+            NumeroConta = numeroConta;
+        }
+
+
+
+        public Conta(string nome, int numeroConta, double saldo) : this(nome, numeroConta) {
+            Saldo = saldo;
+        }
+
+
+        public void AdicionarSaldo(double deposito) {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque) {
+            Saldo -= (saque + 5);
+        }
+        public override string ToString() {
+            return "Conta " + NumeroConta + ", Titular: " + Nome + ", Saldo :$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
+
+
---
+++ Movimentacaobancaria/Movimentacaobancaria/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Movimentacaobancaria {
+    internal class Program {
+        static void Main(string[] args) {
+            Conta novaConta;
+
+            Console.Write("Entre o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (resposta == 's' || resposta == 'S') {
+                Console.Write("Entre o valor do deposito inicial?");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                novaConta = new Conta(nome, conta, depositoInicial);
+            } else {
+                novaConta = new Conta(nome, conta);
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(novaConta);
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            novaConta.AdicionarSaldo(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(novaConta);
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            novaConta.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(novaConta);
+            
+        }
+    }
+}
