f871b2e8963812d93131249474b1c1f5333222b7
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-07-06 22:16:47 -0300

    funcao params

---
+++ FuncaoParams/FuncaoParams/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoParams {
+    internal class Calculadora {
+        public static int Sum(params int[] nums) {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ FuncaoParams/FuncaoParams/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FuncaoParams {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int s1 = Calculadora.Sum(2, 3);
+
+            int s2 = Calculadora.Sum(2, 3,4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
