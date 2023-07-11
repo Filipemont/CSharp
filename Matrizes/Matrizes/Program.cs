e9768b3846a9fc1f7a76ea1aa4e0fa6ad5cc4512
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-07-11 07:25:25 -0300

    matrizes
    pequeno exercício de lógica exemplificando entrada e leitura de matrizes

---
+++ Matrizes/Matrizes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Matrizes {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            double[,] mat = new double[n, n];
+
+            int numerosNegativos = 0;
+            for (int i = 0; i < n; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = double.Parse(valores[j]);
+                    if (mat[i, j] < 0) { numerosNegativos++; };
+                }
+            }
+            Console.WriteLine("Diagonal Principal:");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Números negativos = " + numerosNegativos);
+        }
+    }
+}
