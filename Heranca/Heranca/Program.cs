69398553e7f24abe233fcb691ed491f54109f651
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-07-22 08:52:47 -0300

    herança
    Exemplos de como utilizar herança com C#

---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Heranca {
+    internal class Program {
+        static void Main(string[] args) {
+            Account account = new Account(1001, "Alex", 400.0);
+           // BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 500.0, 500.0);
+
+            //Upcasting
+
+           // Account acc = businessAccount;
+           // Account acc2 = new BusinessAccount(1003, "Bob", 400.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 400.0, 0.01);
+
+            account.Withdraw(15.0);
+            acc3.Withdraw(15.0);
+
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(acc3.Balance);
+
+
+
+
+            //Downcasting
+            
+           // BusinessAccount acc4 = acc2 as BusinessAccount;
+            
+         //   BusinessAccount acc5 = (BusinessAccount)acc3;
+          //  if(acc3 is BusinessAccount) {
+          //     BusinessAccount acc5 = (BusinessAccount)acc3;
+          //      acc5.Loan(200.0);
+          //      Console.WriteLine("Loan!");
+         //   }
+            //if (acc3 is SavingsAccount) {
+              //  SavingsAccount acc5 = acc3 as SavingsAccount;
+              //  acc5.UpdateBalance();
+               // Console.WriteLine("Update!");
+           //*
+
+        }
+    }
+}
+
---
+++ Heranca/Heranca/SavingsAccounte.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca {
+      sealed class SavingsAccount : Account {
+        public double InterestRate { get; set; }
+        public SavingsAccount() { }
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance) {
+            InterestRate = interestRate;
+        }
+        public void UpdateBalance() {
+            Balance += InterestRate * Balance;
+        }
+        public sealed override void Withdraw(double amount) {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
