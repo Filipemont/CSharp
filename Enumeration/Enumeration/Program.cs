0ad37cb0141f380ce08971978642f931f69cca25
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-07-22 18:12:01 -0300

    Enumeration
    Enumeration Basic

---
+++ Enumeration/Enumeration/Program.cs
@@ -0,0 +1,24 @@
+﻿using Enumeration.Entities;
+using Enumeration.Entities.Enums;
+
+namespace Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { 
+            Id = 1920,
+            Moment = DateTime.Now,
+            Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
