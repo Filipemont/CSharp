32e0dc346800e90746c10eb0fa38c5b316691d2d
FilipeMonteiroDev <filipemonteiro73@gmail.com>
2023-07-22 21:32:26 -0300

    Composition
    Exercise using composition solved with teatcher help

---
+++ Composition/Composition/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
