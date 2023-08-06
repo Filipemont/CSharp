72943ef16cd992623f72b76e1ace2b305572ed85
Filipe Monteiro <filipemonteiro73@gmail.com>
2023-08-06 12:13:13 -0300

    Tax calculator
    Calculadora pra taxas de imposto

---
+++ Arquivos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\Windows\temp\file1.txt";
+            string targetPath = @"c:\Windows\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
