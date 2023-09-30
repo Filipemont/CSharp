58e971d55039d6d2e13f6bdf00610908e4e560a3
Filipe Monteiro <filipemonteiro73@gmail.com>
2023-09-30 20:00:47 -0300

    Operacao Linq

---
+++ Linq/Linq/Linq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]numbers = Enumerable.Range(0, 10).ToArray();
+
+
+            Console.WriteLine(numbers.Where(x => x % 2 != 0).Select(x => x*10).ToString());
+
+
+        }
+    }
+}
