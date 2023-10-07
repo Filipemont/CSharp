82c3c9020f68fc685e05fc33e24be627c0057e61
Filipe Monteiro <filipemonteiro73@gmail.com>
2023-10-06 23:38:38 -0300

    Exercicio usando comando Linq

---
+++ Linq/Linq fixacao/ConsoleApp1/ConsoleApp1/Entities/funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    internal class funcionario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public funcionario(string nome, string email, double salario)
+        {
+            Nome = nome;
+            Email = email;
+            Salario = salario;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome +
+                " " +
+                Email +
+                " " +
+                Salario;
+        }
+    }
+}
---
+++ Linq/Linq fixacao/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp1.Entities;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o endereço do arquivo?");
+            string caminho = Console.ReadLine();
+            Console.WriteLine("Qual o salario?");
+            double salario = double.Parse(Console.ReadLine());
+
+            List<funcionario> funcionarios = new List<funcionario>();
+
+            using (StreamReader sr = File.OpenText(caminho))
+            {
+                while(!sr.EndOfStream) {
+                    string[] suporte = sr.ReadLine().Split(',');
+                    string nome = suporte[0];
+                    string mail = suporte[1];
+                    double salarioLista = double.Parse(suporte[2], CultureInfo.InvariantCulture);
+
+                    funcionarios.Add(new funcionario(nome, mail, salarioLista));
+                }
+            }
+
+
+            var email = funcionarios.Where(p => p.Salario > salario).Select(p => p.Email);
+
+
+            Console.WriteLine("Email de quam recebe acima do valor estipulado:");
+            foreach (string emails in email)
+            {
+                Console.WriteLine(emails);
+            }
+
+
+            var somaSalario = funcionarios.Where(p => p.Nome[0] == 'M').Select(p => p.Salario).Sum();
+                
+
+            Console.WriteLine("A soma dos salarios de quem começa o nome com a letra M é : " + somaSalario);
+        }
+    }
+}
